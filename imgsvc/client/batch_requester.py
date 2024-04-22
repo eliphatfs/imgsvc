b237ed35c917fa19aee1f9d1452f863e5af51ab6
eliphatfs <flandre@scarletx.cn>
2024-04-22 14:52:06 -0700

    Add timeout and retry parameters.

--- imgsvc/client/batch_requester.py
+++ imgsvc/client/batch_requester.py
@@ -9,14 +9,20 @@ class BatchRequester(object):
     def __init__(self, endpoint) -> None:
         self.endpoint = endpoint
 
-    def get(self, reqs: List[ProcessRequest]) -> List[bytes]:
+    def get(self, reqs: List[ProcessRequest], timeout=10, retries=2) -> List[bytes]:
         ser = [[req.engine, req.src, req.enc, req.ops] for req in reqs]
-        post = requests.post(self.endpoint, json=ser)
-        if post.status_code != 200:
-            raise ValueError('request failed', post.text)
-        recv = io.BytesIO(post.content)
-        results = []
-        with tarfile.TarFile(fileobj=recv) as tar:
-            for i in range(len(reqs)):
-                results.append(tar.extractfile(str(i)).read())
-        return results
+        for i in range(retries):
+            try:
+                post = requests.post(self.endpoint, json=ser, timeout=timeout)
+            except requests.ConnectionError:
+                if i == retries - 1:
+                    raise
+                continue
+            if post.status_code != 200:
+                raise ValueError('request failed', post.text)
+            recv = io.BytesIO(post.content)
+            results = []
+            with tarfile.TarFile(fileobj=recv) as tar:
+                for i in range(len(reqs)):
+                    results.append(tar.extractfile(str(i)).read())
+            return results
